da714b83180dc818e6e7ce48d641ea0bdd397a1d
lukaferlez <luka.ferlez@gmail.com>
2021-07-18 18:35:04 +0200

    Switched to Microsoft.Data.SqlClient namespace to better support future development & released features.

--- Simpleverse.Dapper/SqlServer/BulkExtensions.cs
+++ Simpleverse.Dapper/SqlServer/BulkExtensions.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Reflection;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Threading.Tasks;
 using Dapper;
 using Dapper.Contrib.Extensions;
