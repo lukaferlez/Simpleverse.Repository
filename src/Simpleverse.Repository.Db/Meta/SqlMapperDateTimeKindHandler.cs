47618051acdae9c26832cdf233607e5751aa0b39
Luka Ferlež <luka.ferlez@gmail.com>
2023-09-01 10:58:24 +0200

    ✨feat: SqlMapper DateTimeKind handler
    Introduced helper class for setting the DateTimeKind on DateTime returned from
    DB.

---
+++ src/Simpleverse.Repository.Db/Meta/SqlMapperDateTimeKindHandler.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using System;
+using System.Data;
+
+namespace Simpleverse.Repository.Db.Meta
+{
+	public class SqlMapperDateTimeKindHandler : SqlMapper.TypeHandler<DateTime>
+	{
+		protected DateTimeKind DateTimeKind { get; }
+
+		public SqlMapperDateTimeKindHandler(DateTimeKind dateTimeKind)
+		{
+			DateTimeKind = dateTimeKind;
+		}
+
+		public override void SetValue(IDbDataParameter parameter, DateTime value)
+		{
+			parameter.DbType = DbType.DateTime;
+			parameter.Value = value;
+		}
+
+		public override DateTime Parse(object value)
+		{
+			return DateTime.SpecifyKind((DateTime)value, DateTimeKind);
+		}
+
+		public static void UseUtc()
+		{
+			SqlMapper.RemoveTypeMap(typeof(DateTime));
+			SqlMapper.AddTypeHandler(new SqlMapperDateTimeKindHandler(DateTimeKind.Utc));
+		}
+	}
+}
