d061f7e9e1feb687a9edf96be0e02b2bbc0d21e6
Luka Ferlež <luka.ferlez@gmail.com>
2023-09-15 20:10:30 +0200

    🖥️build: Added missing model class

---
+++ src/Simpleverse.Repository.Db.Test/Model.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Simpleverse.Repository.Db.Test
+{
+	public class Model
+	{
+		public int Int { get; set; }
+		public int? IntNullable { get; set; }
+		public @Enum EnumValue { get; set; }
+		public Enum? EnumValueNullable { get; set; }
+		public DateTime DateTime { get; set; }
+		public DateTime? DateTimeNullable { get; set; }
+		public string String { get; set; }
+		public bool Bool { get; set; }
+		public bool? BoolNullable { get; set; }
+	}
+}
