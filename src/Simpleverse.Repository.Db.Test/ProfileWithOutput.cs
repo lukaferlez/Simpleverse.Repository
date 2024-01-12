bb01dbe3d57c0d6f824487c34f724ee6e6e38c34
Filip Klarić <filip.klaric@bozic.io>
2024-01-12 11:36:08 +0100

    Extracted Profile to Simpleverse.Repository.Db

---
+++ src/Simpleverse.Repository.Db.Test/ProfileWithOutput.cs
@@ -0,0 +1,21 @@
+﻿using StackExchange.Profiling;
+using Xunit.Abstractions;
+
+namespace Simpleverse.Repository.Db.Test
+{
+	public class ProfileWithOutput : Profile
+	{
+		private readonly ITestOutputHelper _output;
+
+		public ProfileWithOutput(ITestOutputHelper output, string profilerName = null) : base(profilerName) 
+		{
+			_output = output;
+		}
+
+		public override void OverridableDispose()
+		{
+			base.OverridableDispose();
+			_output.WriteLine(RenderProfiler(MiniProfiler.Current));
+		}
+	}
+}
